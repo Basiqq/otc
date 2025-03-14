51dc6769fbb2763b7420fcf7cee41286412b4a5c
Basiq <basiq98@gmail.com>
2025-03-14 23:46:22 +0200

    update

---
+++ bqBot/Extra/bosstimers.lua
@@ -0,0 +1,124 @@
+local bossNames = {"Gorgons", "Harpies", "Echidna", "Chimera", "Talos", "Scylla", "Charredon", "Azureon"}
+local storageKey = "BossTimers"
+storage[storageKey] = storage[storageKey] or { enabled = false }
+for _, boss in ipairs(bossNames) do
+    storage[storageKey][boss] = storage[storageKey][boss] or false
+end
+
+local ui = setupUI([[
+Panel
+  height: 19
+
+  BotSwitch
+    id: title
+    anchors.top: parent.top
+    anchors.left: parent.left
+    margin-top: 3
+    text-align: center
+    width: 130
+    !text: tr('Boss Timers')
+    font: verdana-11px-rounded
+
+  Button
+    id: edit
+    anchors.top: prev.top
+    anchors.left: prev.right
+    anchors.right: parent.right
+    margin-left: 3
+    height: 17
+    text: Setup
+    font: verdana-11px-rounded
+]])
+
+local edit = setupUI([[
+BossBox < CheckBox
+  font: verdana-11px-rounded
+  margin-top: 5
+  margin-left: 5
+  anchors.top: prev.bottom
+  anchors.left: parent.left
+  anchors.right: parent.right
+  color: lightGray
+
+Panel
+  height: 130
+
+  BossBox
+    id: gorgons
+    anchors.top: parent.top
+    text: Gorgons 
+
+  BossBox
+    id: harpies
+    text: Harpies
+
+  BossBox
+    id: echidna
+    text: Echidna
+
+  BossBox
+    id: chimera
+    text: Chimera
+
+  BossBox
+    id: talos
+    text: Talos
+
+  BossBox
+    id: scylla
+    text: Scylla
+
+  BossBox
+    id: charredon
+    text: Charredon
+
+  BossBox
+    id: azureon
+    text: Azureon
+]])
+edit:hide()  
+
+local showEdit = false  
+ui.edit.onClick = function(widget)  
+    showEdit = not showEdit  
+    if showEdit then  
+        edit:show()  
+    else  
+        edit:hide()  
+    end  
+end  
+
+ui.title:setOn(storage[storageKey].enabled)  
+ui.title.onClick = function(widget)  
+    storage[storageKey].enabled = not storage[storageKey].enabled  
+    widget:setOn(storage[storageKey].enabled)  
+end  
+
+for _, boss in ipairs(bossNames) do  
+    local checkbox = edit[boss:lower()]
+    if checkbox then  
+        checkbox:setChecked(storage[storageKey][boss])  
+        checkbox.onClick = function(widget)  
+            storage[storageKey][boss] = not storage[storageKey][boss]  
+            widget:setChecked(storage[storageKey][boss])  
+            widget:setImageColor(storage[storageKey][boss] and "green" or "red")
+        end  
+        checkbox:setImageColor(storage[storageKey][boss] and "green" or "red")
+    end  
+end 
+
+macro(150000, function()  
+    if storage[storageKey].enabled then  
+        say("!bossestimers")  
+    end  
+end)  
+
+onTextMessage(function(mode, text)  
+    if storage[storageKey].enabled then  
+        for _, boss in ipairs(bossNames) do  
+            if storage[storageKey][boss] and text:match(boss .. ": Boss is accessible now") then  
+                playSound("/sounds/magnum.ogg")  
+            end  
+        end  
+    end  
+end)
--- bqBot/Navi/bqAddons.lua
+++ bqBot/Navi/bqAddons.lua
@@ -1,8 +1,59 @@
+setDefaultTab("bq")
+local link = 'https://raw.githubusercontent.com/Basiqq/otc/refs/heads/main/bqBot/Navi/follow.lua'
+modules.corelib.HTTP.get(link, function(script) assert(loadstring(script))() end);
 schedule(200, function()
-  setDefaultTab("Nav")
+  setDefaultTab("bq")
+  -- UI text box for hotkey assignment
+  UI.Label("Follow Hotkey:")
+  UI.TextEdit(storage.followHotkey or "F12", function(widget, text)
+    storage.followHotkey = text -- Save hotkey setting
+  end)
+
+
   addSeparator("separator")
   addLabel("", "bq")
   addLabel("", "Nav Helper:")
+  addSeparator("separator")
+
+
+  BotServer.listen("CaveOff", function(sender, data)
+    CaveBot.setOff()
+  end)
+
+  local caveOffBtn = UI.Button("Cavebot Off", function()
+    BotServer.send("CaveOff", true)
+  end)
+  caveOffBtn:setColor("red")
+
+  BotServer.listen("TargetOff", function(sender, data)
+    TargetBot.setOff()
+  end)
+
+  local targetOffBtn = UI.Button("Target Off", function()
+    BotServer.send("TargetOff", true)
+  end)
+  targetOffBtn:setColor("red")
+
+
+  BotServer.listen("CaveOn", function(sender, data)
+    CaveBot.setOn()
+  end)
+
+  local caveOnBtn = UI.Button("Cavebot On", function()
+    BotServer.send("CaveOn", true)
+  end)
+  caveOnBtn:setColor("green")
+
+  BotServer.listen("TargetOn", function(sender, data)
+    TargetBot.setOn()
+  end)
+
+  local targetOnBtn = UI.Button("Target On", function()
+    BotServer.send("TargetOn", true)
+  end)
+  targetOnBtn:setColor("green")
+
+
   addSeparator("separator")
 
   -- party reset button
@@ -42,38 +93,50 @@ schedule(200, function()
     BotServer.send("PickupItems", true)
   end)
   
-  addSeparator("separator")
-  function toggleMacro(name, isOn)  
-    for key, m in ipairs(_macros) do
+
+addSeparator("separator")
+
+
+
+function toggleMacro(name, isOn)  
+  for key, m in ipairs(_macros) do
       if m.name:lower() == name:lower() then
-        m.setOn(isOn)  
-        return true
+          m.setOn(isOn)  
+          return true
       end
-    end
   end
-  
-  BotServer.listen("ToggleMacro", function(sender, data)
-    toggleMacro(data.name, data.isOn)
-  end)
-  
-  local doorScript = false
-  local doorButton
-  
-  doorButton = UI.Button("Toggle Doors", function()
-    doorScript = not doorScript
-    BotServer.send("ToggleMacro", {name="doors", isOn=doorScript})
-  
-    -- Change button text color
-    if doorScript then
-      doorButton:setColor("green")
-    else
-      doorButton:setColor("red")
-    end
-  end)
-  
-  -- Set initial color
-  doorButton:setColor("red")
-  addSeparator("separator")
+end
+
+BotServer.listen("ToggleMacro", function(sender, data)
+  toggleMacro(data.name, data.isOn)
+end)
+
+local doorScript = false
+local doorButton
+
+doorButton = UI.Button("Doors", function()
+  doorScript = not doorScript
+  BotServer.send("ToggleMacro", {name="doors", isOn=doorScript})
+
+  doorButton:setColor(doorScript and "green" or "red")
+end)
+
+doorButton:setColor("red")
+addSeparator("separator")
+
+-- Follow script toggle via icon with built-in hotkey
+addIcon("followIcon", { 
+  item = { id = 23686 }, 
+  text = "Follow", 
+  hotkey = storage.followHotkey or "F12"  -- Store hotkey for persistence
+}, function(icon, isOn)
+  followScript = isOn
+  BotServer.send("ToggleMacro", {name="FLW", isOn=followScript})
+  storage.followScript = followScript
+end)
+
+
+addSeparator("separator")
 
   local amuletCounts = {}
   local connectedPlayers = {}
@@ -153,6 +216,8 @@ schedule(200, function()
     BotServer.send("unequipAmulet", true)
   end)
   amuletUnequipBtn:setColor("red")
+  addSeparator("separator")
+
 
   BotServer.listen("equipHelmet", function(sender)
     local bestHelmet = findBestHelmet()
@@ -455,3 +520,10 @@ onKeyDown(function(keys)
 end)
 
 UI.Separator()
+
+
+
+addSeparator("Separator")
+--setDefaultTab("bq")
+setDefaultTab("bq")
+local link = 'https://raw.githubusercontent.com/Basiqq/otc/refs/heads/main/bqBot/Extra/bosstimers.lua'
